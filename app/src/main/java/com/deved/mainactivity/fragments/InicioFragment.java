bc10e7e7b2d45f130735413fd2d8e2921840777d
Your Name <you@example.com>
2019-03-16 12:57:35 -0500

    mapas

--- app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
+++ app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
@@ -1,18 +1,37 @@
 package com.deved.mainactivity.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.deved.mainactivity.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class InicioFragment extends Fragment {
+public class InicioFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    private static final int REQUEST_PERMISSION_LOCATION = 200;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private Marker marcadorUbicacionActual;
+    Context context;
 
 
     public InicioFragment() {
@@ -25,6 +44,19 @@ public class InicioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicio, container, false);
+
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
