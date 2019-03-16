be6f19bf10bc1bda63044b720838d5b31fb152dc
Your Name <you@example.com>
2019-03-16 13:28:45 -0500

    mapasinicio

--- app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
+++ app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
@@ -15,10 +15,13 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,20 +46,32 @@ public class InicioFragment extends SupportMapFragment implements OnMapReadyCall
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
 
-
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
         getMapAsync(this);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+        return rootView;
+
+
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+// Posicionar el mapa en una localización y con un nivel de zoom
+        LatLng latLng = new LatLng(36.679582, -5.444791);
+        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
+        // callejero es 17 aprox.
+        float zoom = 13;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
 
+        // Colocar un marcador en la misma posición
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        // Más opciones para el marcador en:
+        // https://developers.google.com/maps/documentation/android/marker
+
+        // Otras configuraciones pueden realizarse a través de UiSettings
+        // UiSettings settings = getMap().getUiSettings();
     }
 }
+
