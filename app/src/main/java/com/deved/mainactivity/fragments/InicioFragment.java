cbdf47d14650fe18fb46cc618e10e2e05a9ccd49
Prado <pradoulima@gmail.com>
2019-03-16 17:19:26 -0500

    HOla.Edmundo

--- app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
+++ app/src/main/java/com/deved/mainactivity/fragments/InicioFragment.java
@@ -1,7 +1,5 @@
 package com.deved.mainactivity.fragments;
 
-
-import android.content.Context;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
@@ -10,66 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.deved.mainactivity.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class InicioFragment extends SupportMapFragment implements OnMapReadyCallback {
+public class InicioFragment extends Fragment {
 
-    private static final int REQUEST_PERMISSION_LOCATION = 200;
-    private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
-    private Marker marcadorUbicacionActual;
-    Context context;
+        public InicioFragment() {
+            // Required empty public constructor
+        }
 
 
-    public InicioFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-
-        getMapAsync(this);
-
-        return rootView;
-
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_inicio, container, false);
+        }
 
     }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-// Posicionar el mapa en una localización y con un nivel de zoom
-        LatLng latLng = new LatLng(36.679582, -5.444791);
-        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
-        // callejero es 17 aprox.
-        float zoom = 13;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
-
-        // Colocar un marcador en la misma posición
-        mMap.addMarker(new MarkerOptions().position(latLng));
-        // Más opciones para el marcador en:
-        // https://developers.google.com/maps/documentation/android/marker
-
-        // Otras configuraciones pueden realizarse a través de UiSettings
-        // UiSettings settings = getMap().getUiSettings();
-    }
-}
-
