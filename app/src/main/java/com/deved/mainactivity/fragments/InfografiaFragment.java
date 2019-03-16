2569f143f1b8899dbb4e5df102f2168982eecbfe
Your Name <you@example.com>
2019-03-16 17:34:09 -0500

    modificadoultimo

--- app/src/main/java/com/deved/mainactivity/fragments/InfografiaFragment.java
+++ app/src/main/java/com/deved/mainactivity/fragments/InfografiaFragment.java
@@ -2,29 +2,18 @@ package com.deved.mainactivity.fragments;
 
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.deved.mainactivity.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InfografiaFragment extends Fragment  {
-
-    private View rootView;
-    private MapView mapView;
-    private GoogleMap gMap;
 
     public InfografiaFragment() {
         // Required empty public constructor
@@ -35,16 +24,10 @@ public class InfografiaFragment extends Fragment  {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_infografia, container, false);
-        return view;
+       return inflater.inflate(R.layout.fragment_infografia, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-
-    }
 
 
 }
